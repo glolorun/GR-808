2cf8736f5ef2378d75ff494f3fd05e580369daa8
glolorun <olukareem@gmail.com>
2021-08-27 00:03:44 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InterfaceComponent } from './Components/interface/interface.component';
+import { RowComponent } from './Components/row/row.component';
+import { ButtonComponent } from './Components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InterfaceComponent,
+    RowComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
